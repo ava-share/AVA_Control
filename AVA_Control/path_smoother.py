55104a151ffddecc3998fff3a40e7ecb0ae13fae
tvidano <61514829+tvidano@users.noreply.github.com>
2023-02-28 14:39:50 -0800

    Path smoother draft 2. Working in simulation.
    Signed-off-by: tvidano <61514829+tvidano@users.noreply.github.com>

--- AVA_Control/path_smoother.py
+++ AVA_Control/path_smoother.py
@@ -1,7 +1,7 @@
 """A simple cubic spline local planner."""
 
 import argparse
-from autoware_msgs.msg import LaneArray
+from autoware_msgs.msg import LaneArray, Lane, Waypoint
 from geometry_msgs.msg import Point, Pose, PoseStamped, Quaternion, Vector3
 import matplotlib.pyplot as plt 
 import numpy as np
@@ -29,7 +29,9 @@ class ROSLocalPlanner:
         rospy.loginfo('Initialized local_planner.')
         rospy.on_shutdown(self.MyHook)
         self.rate = rospy.Rate(10)
-        self.converge_distance = 10 # [m]
+        self.converge_distance = 20 # [m]
+        self.plan_distance = 25  # [m]
+        self.wheelbase = 2.9 # [m]
 
         self.ego_pose = None
         self.global_plan = None
@@ -40,7 +42,8 @@ class ROSLocalPlanner:
         rospy.Subscriber("/current_pose", PoseStamped, 
 						 callback=self.CurrentPoseCallback, queue_size=1)
 
-        self.markers = rviz_tools.RvizMarkers('/map', 'local_plan')
+        self.path_markers = rviz_tools.RvizMarkers('/map', 'local_plan')
+        self.path_direction_markers = rviz_tools.RvizMarkers('/map', 'local_direction')
 
         rospy.loginfo('Begin publishing reference poses.')
         while not rospy.is_shutdown():
@@ -80,61 +83,127 @@ class ROSLocalPlanner:
         ego_pose_np = self.pose_to_np(ego_pose)
 
         # Store waypoints that are far from the ego position.
-        # TODO: Fix bug where points are allowed behind the vehicle.
-        feasible_waypoints = []
-        for waypoint in desired_lane.waypoints:
-            waypoint_pose_np = self.pose_to_np(waypoint.pose.pose)
-            difference = waypoint_pose_np - ego_pose_np
-            distance_to_ego = np.linalg.norm(difference[0:2], axis=0)
-            if distance_to_ego > self.converge_distance:
-                feasible_waypoints.append(waypoint_pose_np)
+        feasible_waypoints = self.get_feasible_waypoints(desired_lane, 
+                                                         ego_pose_np)
 
-        # Rotate waypoints and ego position to a coordinate axis aligned
-        # with the vector from the ego position and the closest feasible
-        # waypoint to enforce CubicSpline's need for increasing x.
-        # TODO: Fix bug where the local path is separated from the ego vehicle
-        # and the offset appears to be a function of the initial position.
-        theta = np.arctan2(feasible_waypoints[0][1,0] - ego_pose_np[1,0],
-                           feasible_waypoints[0][0,0] - ego_pose_np[0,0])
+        # Fit a spline from ego pose to the feasible waypoints.
+        local_plan, yaw_ref = self.fit_spline(ego_pose_np, feasible_waypoints)
+        path_points, path_quats, path_poses = self.convert_to_ros(local_plan, 
+                                                                  yaw_ref)
+
+        # Create RVIZ path for visualization.
+        self.publish_rviz_path(path_points, path_poses)
+
+        # Publish to /final_waypoints
+        pub_path = rospy.Publisher("/final_waypoints", Lane, queue_size=1)
+        local_path = Lane()
+        # Convert poses to waypoints.
+        waypoints = []
+        for pose in path_poses:
+            w = Waypoint()
+            w.pose.pose = pose
+            waypoints.append(w)
+        local_path.header.stamp = rospy.get_rostime()
+        local_path.waypoints = waypoints
+
+        pub_path.publish(local_path)
+
+    def fit_spline(self, ego_pose, waypoints):
+        '''Creates a cubic spline connecting the ego_pose to waypoints.
+        
+        Rotates the ego_pose, front axle, and waypoints to a coordinate
+        axis parallel to the vector from the ego position and the closest
+        waypoint to enforce CubicSpline's need for increasing x.
+        '''
+        # Compute rotation matrices.
+        theta = np.arctan2(waypoints[0,1] - ego_pose[1],
+                           waypoints[0,0] - ego_pose[0])
         R = np.array([[np.cos(theta), -np.sin(theta)],
                       [np.sin(theta), np.cos(theta)]])
         R_inv = np.linalg.inv(R)
 
+        # Compute front axle position.
         front_axle = np.array([
-            [ego_pose_np[0,0] + 2.9*np.cos(ego_pose_np[2,0])],
-            [ego_pose_np[1,0] + 2.9*np.sin(ego_pose_np[2,0])],
+            [ego_pose[0] + self.wheelbase*np.cos(ego_pose[2])],
+            [ego_pose[1] + self.wheelbase*np.sin(ego_pose[2])],
             [0]
         ])
-        feasible_waypoints.insert(0, front_axle)
-        feasible_waypoints.insert(0, ego_pose_np)
 
-        rotated_feasible_points = [R_inv.dot(point[0:2]) for \
-                                   point in feasible_waypoints]
-        rotated_feasible_points = np.array(rotated_feasible_points)
-        points = np.array(feasible_waypoints)
+        # Insert front axle and ego position to enforce heading constraint.
+        waypoints = np.vstack((ego_pose.reshape((1,3)), 
+                               front_axle.reshape((1,3)), 
+                               waypoints))
+        
+        # Rotate waypoints.
+        rotated_waypoints = [R_inv.dot(p[0:2]) for p in waypoints]
+        rotated_waypoints = np.array(rotated_waypoints)
 
-        # Fit spline from ego position to closest waypoint.
-        local_plan = CubicSpline(rotated_feasible_points[:,0,0],
-                                 rotated_feasible_points[:,1,0],
-                                 bc_type=((2, 0),
-                                          (2, 0)))
-        local_plan_x = np.linspace(points[0,0],points[0,0] + 50)
-        local_plan_y = local_plan(local_plan_x)
-        local_plan_positions = np.vstack((local_plan_x, local_plan_y))
+        # Fit spline.
+        spline = CubicSpline(rotated_waypoints[:,0],
+                             rotated_waypoints[:,1],
+                             bc_type='natural')
+        plan_x = np.linspace(rotated_waypoints[0,0],
+                             rotated_waypoints[0,0] + self.plan_distance)
+        plan_y = spline(plan_x)
+        plan_dy = spline(plan_x, 1)
+        plan = np.vstack((plan_x, plan_y))
+        plan_direction = np.vstack((np.ones(plan_dy.shape), plan_dy))
 
         # Rotate back to global coordinate system.
-        local_plan_global = [R.dot(point) for point in local_plan_positions.T]
+        plan_unrotated = [R.dot(p) for p in plan.T]
+        plan_direction_unrotated = [R.dot(p) for p in plan_direction.T]
+        plan_yaw = [np.arctan2(p[1],p[0]) for p in plan_direction_unrotated]
 
-        # Create RVIZ path for visualization.
-        path = []
-        for position in local_plan_global:
-            path.append(Point(position[0],position[1],0))
-        width = 0.02
-        self.markers.publishPath(path, 'orange', width, 0.1)
+        return (plan_unrotated, plan_yaw)
+
+    def convert_to_ros(self, positions, yaws):
+        '''Converts positions to Points and yaws to Quaternions.'''
+        path = [Point(p[0],p[1],0) for p in positions]
+        orientation = [quaternion_from_euler(0, 0, yaw) for yaw in yaws]
+        direction = [Quaternion(q[0], q[1], q[2], q[3]) for q in orientation]
+        poses = [Pose(p,q) for p,q in zip(path, direction)]
+        return path, direction, poses
+
+    def publish_rviz_path(self, path, poses):
+        '''Publishes markers for RVIZ visualization of the local path.'''
+        width = 0.1
+        self.path_markers.publishPath(path, 'orange', width, 0.1)
+        arrow_length = 2.0
+        self.path_direction_markers.publishArrow(poses[1], 'pink', arrow_length, 0.1)
+
+    def get_feasible_waypoints(self, lane, ego_pose):
+        '''Returns the waypoints in front of the ego vehicle without the
+        nearest waypoints.'''
+        waypoints = []
+        for waypoint in lane.waypoints:
+            waypoint_pose_np = self.pose_to_np(waypoint.pose.pose)
+            waypoints.append(waypoint_pose_np)
+        waypoints = np.array(waypoints)
+        # Find closest waypoint.
+        ego_to_waypoints = waypoints - ego_pose
+        distances = np.linalg.norm(ego_to_waypoints[:,:2], axis=1)
+        closest_i = np.argmin(distances)
+        # Assume global path is in increasing order, filter out points behind
+        # closest waypoint.
+        # Remove waypoints past the plan distance.
+        distant_i = np.argmin(abs(distances[closest_i:] - self.plan_distance))
+        distant_i += closest_i
+        # Remove waypoints within the convergence distance.
+        convergence_i = np.argmin(abs(distances[closest_i:] - \
+                                      self.converge_distance))
+        convergence_i += closest_i
+        return waypoints[convergence_i:distant_i+1]
+
+    @staticmethod
+    def angle(vector_1, vector_2):
+        '''Computes the angle from vector_1 to vector_2.'''
+        angle_2 = np.arctan2(vector_2[1,0], vector_2[0,0])
+        angle_1 = np.arctan2(vector_1[1,0], vector_1[0,0])
+        return angle_2 - angle_1
 
     @staticmethod
     def pose_to_np(pose):
-        """Converts pose to 3x1 numpy array."""
+        '''Converts pose to 3x1 numpy array.'''
         x = pose.position.x
         y = pose.position.y
         q = pose.orientation
@@ -145,7 +214,7 @@ class ROSLocalPlanner:
             yaw -= 2*np.pi
         if yaw < -np.pi:
             yaw += 2*np.pi
-        return np.array([[x],[y],[yaw]])
+        return np.array([x, y, yaw])
     
     @classmethod
     def waypoints_to_np(cls, waypoints):
@@ -162,12 +231,3 @@ if __name__ == '__main__':
         ROSLocalPlanner()
     except rospy.ROSInterruptException:
         pass
-
-    # ctr_points = np.array(ctr_points)
-    # cs = CubicSpline(ctr_points[:,0],ctr_points[:,1],
-    #                  bc_type=((1, -65/180*np.pi), (1, -40/180*np.pi)))
-    # eval_points_x = np.linspace(min(ctr_points[:,0]),max(ctr_points[:,0]))
-    # eval_points_y = cs(eval_points_x)
-    # plt.plot(eval_points_x, eval_points_y)
-    # plt.scatter(ctr_points[:,0],ctr_points[:,1], c='black')
-    # plt.show()
